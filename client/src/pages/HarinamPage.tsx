1a04f5655e71b501c177a33ffe39d0fe92796661
Kranthi Ramireddy <kranthiramireddy@gmail.com>
2026-06-01 08:51:33 +0530

    leaderboard loading to 20

--- client/src/pages/HarinamPage.tsx
+++ client/src/pages/HarinamPage.tsx
@@ -399,7 +399,7 @@ export default function HarinamPage() {
     if (loading) return
     setLoading(true)
     try {
-      const data = await getLeaderboard(p, 5)
+      const data = await getLeaderboard(p, 20)
       setLeaderboard((prev) => append ? [...prev, ...data.leaderboard] : data.leaderboard)
       setHasMore(data.pagination.hasMore)
       setPage(p)
